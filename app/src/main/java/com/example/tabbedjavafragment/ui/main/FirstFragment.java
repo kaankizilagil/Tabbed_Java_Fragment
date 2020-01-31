6b328ed8c59487beb934303def65c3d445e6a570
kaankizilagil <kaankizilagil@gmail.com>
2020-01-31 14:46:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tabbedjavafragment/ui/main/FirstFragment.java
@@ -0,0 +1,69 @@
+package com.example.tabbedjavafragment.ui.main;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.tabbedjavafragment.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+public class FirstFragment extends Fragment {
+
+    PageViewModel pageViewModel;
+
+    public static FirstFragment newInstance() {
+
+        return new FirstFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Toast.makeText(getActivity().getApplicationContext(),"Welcome, Please Enter Your Name !", Toast.LENGTH_LONG).show();
+
+        pageViewModel = ViewModelProviders.of(requireActivity()).get(PageViewModel.class);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        EditText editText = view.findViewById(R.id.editText);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                pageViewModel.setName(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+}
